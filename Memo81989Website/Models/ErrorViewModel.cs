7651d6849b2a80a6b6347ae2c6808630b6ad0fe4
Guillermo Salas <memo81989@gmail.com>
2022-05-25 11:25:53 -0600

    Proyecto integrado

---
+++ Memo81989Website/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Memo81989Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
